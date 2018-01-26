44706a9f744c7ecaa873282b986b0847d2197cb6
Shiv Verma <shiverma@microsoft.com>
2018-01-26 15:11:07 -0800

    small fix

--- install.sh
+++ install.sh
@@ -11,10 +11,10 @@ if [ $? -ne 0 ]; then # The data disk is not mounted at drive1
     mount /dev/disk/azure/scsi1/lun0 /drive1
     if [ $? -ne 0 ]; then # Manual mount failed. Let's format it
         mkfs.ext4 -F /dev/disk/azure/scsi1/lun0
+        date > /drive1/formattime
     fi
     echo "/dev/disk/azure/scsi1/lun0 /drive1 ext4 defaults,nofail 0 2" >> /etc/fstab
     mount /drive1
-    date > /drive1/formattime
 fi
 
 #check if the systemd module is there
