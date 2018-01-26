138a8cf8e22cadab5376d17968215b17ce007ef7
Shiv Verma <shiverma@microsoft.com>
2018-01-26 15:02:41 -0800

    add some pre-format checks

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+mkdir -p /drive1
+mount /drive1
 # Check that the disk is attached
 mountpoint /drive1
 if [ $? -ne 0 ]; then # The data disk is not mounted at drive1
+
     echo "mounted drive not found at /drive1"
-    mkfs.ext4 -F /dev/disk/azure/scsi1/lun0
-    mkdir -p /drive1
+    # lets first try to manually mount it
+    mount /dev/disk/azure/scsi1/lun0 /drive1
+    if [ $? -ne 0 ]; then # Manual mount failed. Let's format it
+        mkfs.ext4 -F /dev/disk/azure/scsi1/lun0
+    fi
     echo "/dev/disk/azure/scsi1/lun0 /drive1 ext4 defaults,nofail 0 2" >> /etc/fstab
     mount /drive1
+    date > /drive1/formattime
 fi
 
 #check if the systemd module is there
